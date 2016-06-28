a37442ea55cc54315755a72626b4a500eddea408
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-06-28 04:28:39 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 /*Variables*/
 $(document).ready(function() {
 	init_map();
+	$('.video').height($('.memorias img').height()*.7);
+	$('.h_ribbon').height($('.memorias img').height()*.7);
+	$('.menciones').height($('.memorias img').height()+$('.h_ribbon').height()+6);
+});
+$(window).resize( function () {
+	$('.video').height($('.memorias img').height()*.7);
+	$('.h_ribbon').height($('.memorias img').height()*.7);
+	$('.menciones').height($('.memorias img').height()+$('.h_ribbon').height()+6);
 });
