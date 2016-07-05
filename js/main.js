9447f2dd443abe57b5ba610a93d8215e7b691ba8
Gerardo Nille <gerardonille@gmail.com>
2016-07-05 14:40:08 -0500

    centros animacion básica

--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@
 
 function slide_Centros(centro, caller) {
     //$(caller).closest('#centro1').slideUp;
+
+    $(caller).parent().parent().toggleClass("faa-horizontal animated faa-fast");
+    setTimeout(function() {
+    $(caller).parent().parent().toggleClass("faa-horizontal animated faa-fast");
     $(caller).parent().parent().slideToggle();
+    if ($('#' + centro).is(':visible')) {
+        $("#" + centro).hide(1500);
+        //$(".banner_servicio").removeClass("imagen_animacion");
+    } else {
+        //$(".banner_servicio").addClass("imagen_animacion");
+        //slide_Check();
+        $(".centro_expandido").not(centro).slideUp("1500");
+        $("#" + centro).slideDown(1500);
+    }
+}, 500);
+    /*$(caller).parent().parent().delay(5000).queue(function(){
+  $(caller).parent().parent().toggleClass("faa-bounce animated");
+});*/
+
+
+    /*$(caller).parent().parent().slideToggle();
     if ($('#' + centro).is(':visible')) {
         $("#" + centro).slideUp(1500);
         //$(".banner_servicio").removeClass("imagen_animacion");
@@ -11,11 +31,14 @@ function slide_Centros(centro, caller) {
         //slide_Check();
         $(".centro_expandido").not(centro).slideUp("1500");
         $("#" + centro).slideDown(1500);
-    }
+    }*/
 }
 function show_banner(banner, caller) {
     //$(caller).closest('#centro1').slideUp;
+
     $(caller).parent().parent().parent().slideToggle();
+    $(caller).parent().parent().parent().toggleClass("faa-passing animated faa-fast");
+    $(caller).parent().parent().parent().toggleClass("faa-passing-reverse animated faa-fast");
     $("#" + banner).slideToggle();
     //$(".banner_servicio").removeClass("imagen_animacion");
 }
